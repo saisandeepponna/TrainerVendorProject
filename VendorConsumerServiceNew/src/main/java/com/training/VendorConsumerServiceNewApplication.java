45ae26c62b0ad6630415da693850c761ca786e1e
saisandeepponna <ponna.sandeep2@gmail.com>
2021-04-18 20:40:06 +0530

    Spring Boot Project Vendor Producer First commit

---
+++ VendorConsumerServiceNew/src/main/java/com/training/VendorConsumerServiceNewApplication.java
@@ -0,0 +1,21 @@
+package com.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@EnableFeignClients
+@EnableDiscoveryClient
+//@EnableSwagger2
+@SpringBootApplication
+public class VendorConsumerServiceNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorConsumerServiceNewApplication.class, args);
+	}
+
+}
